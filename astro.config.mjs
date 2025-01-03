de7430472b3fd8e033fffc0bda621b3c936794e7
subject026 <subject026@protonmail.com>
2025-01-03 08:58:38 +0000

    static output config + trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   integrations: [tailwind(), react()],
   image: {
     // Used for all `<Image />` and `<Picture />` components unless overridden
